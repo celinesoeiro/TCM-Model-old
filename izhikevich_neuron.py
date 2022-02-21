badb566112accc5d9b3dfa25ca09ee940e34ba4c
Celine Soeiro <celinesoeiro@gmail.com>
2022-02-20 22:01:41 -0300

    original izhikevish model

--- izhikevich_neuron.py
+++ izhikevich_neuron.py
@@ -1,59 +1,105 @@
-# -*- coding: utf-8 -*-
 """
-Created on Sun Feb 20 19:12:42 2022
 
-@author: celin
+@author: Celine Soeiro
+
+@description: Izhikevich Model
+
+The Izhikevich model can be represented through an 2-D system of differential 
+equations:
+    
+    dvdt = 0.04*v*v + 5*v + 140 - u + I
+    dudt = a*(b*v - u)
+    
+    with conditions: 
+        if v >= 30mV, then v = c and u = u + d
+
 """
+
+# =============================================================================
+# CONSTANTS DEFINITIONS
+# 
+# u: Equation variable - Represents membrane recovery variable
+# v: Equation variable - Represents membrane potential of the neuron
+# a: Equation parameter - Time scale of the recovery variable u
+# b: Equation parameter - Sensitivity of u to the fluctuations in v
+# c: Equation parameter - After-spike reset value of v
+# d: Equation parameter - After-spike reset value of u
+#
+# excitatory_neurons: Number of excitatory neurons
+# inhibitory_neurons: Number of inhibitory neurons
+# excitatory_vector: Column vector of excitatory neurons
+# inhibitory_vector: Column vector of inhibitory neurons
+# 
+# =============================================================================
 
 import numpy as np
 import pylab as plt
 
-C = 150
-Vr = -75
-Vl = -45
-k = 1.2
-a = 0.01
-b = 5
-c = -56
-d = 130
-Vpico = 50
+time_steps = 1000
+excitatory_neurons = 800
+inhibitory_neurons = 200
+neurons = excitatory_neurons + inhibitory_neurons
+excitatory_vector = np.random.rand(excitatory_neurons, 1)
+inhibitory_vector = np.random.rand(inhibitory_neurons, 1)
 
-# Modelo Izhikevich
+a = np.concatenate([
+    0.02*np.ones((excitatory_neurons, 1)),  # excitatory contribution
+    0.02 + 0.08*inhibitory_vector           # inhibitory contribution
+    ])
+b = np.concatenate([
+    0.2*np.ones((excitatory_neurons, 1)),   # excitatory contribution
+    0.25 - 0.05*inhibitory_vector           # inhibitory contribution
+    ])
+c = np.concatenate([
+    -65 + 15*excitatory_vector**2,          # excitatory contribution
+    -65*np.ones((inhibitory_neurons, 1))    # inhibitory contribution
+    ])
+d = np.concatenate([
+    8 - 6*excitatory_vector**2,             # excitatory contribution
+    2*np.ones((inhibitory_neurons, 1))      # inhibitory contribution
+    ])
+S = np.concatenate([
+    0.5*np.random.rand(neurons, excitatory_neurons),
+    -np.random.rand(neurons,inhibitory_neurons)
+    ], axis = 1)
 
-def dvdt(V,I,u):
-    return (k*(V - Vr)*(V - Vl) - u + I)/C 
 
-def dudt(V,u):
-    return a*(b*(V - Vr) - u)
+v = -65*np.ones((neurons, 1))
+u = b*-65
+neurons_that_fired_across_time = []
+voltage_across_time = []
 
-tmin = 0
-tmax = 500
-dt = 1e-3
-t = np.arange(tmin,tmax,dt)
+for t in range(1, time_steps + 1):
+    # The input voltage
+    I = np.concatenate([
+        5*np.random.rand(excitatory_neurons, 1),
+        2*np.random.rand(inhibitory_neurons, 1)
+        ])
 
-
-J = np.zeros(len(t))
-v = np.zeros(len(t))
-u = np.zeros(len(t))
-
-J_values = [300,370,500,550]
-
-for j in range(len(J_values)):
-    J[20000:450000] = J_values[j]
-    for i in range (len(t) - 1):
-        v[i + 1] = dvdt(v[i], J[i], u[i]) + dt*dvdt(v[i], J[i], u[i])
-        u[i + 1] = dudt(v[i], u[i]) + dt*dudt(v[i], u[i])
-
-    fig, axs = plt.subplots(2, sharex=False, figsize=(7,10))
-    fig.suptitle(f'Questao 6 - letra a - ii', fontsize=15)
-    axs[0].set_title('V')
-    axs[0].set(ylabel='Voltagem (mV)')
-    axs[0].set(xlabel='tempo (ms)')
-    axs[0].grid()
-    axs[0].plot(t, v, 'k')
+    neurons_that_fired = np.where(v > 30)
+    voltage_across_time.append(float(v[10]))
+    neurons_that_fired_across_time.append([
+        t + 0 * neurons_that_fired[0], 
+        neurons_that_fired[0]
+        ])
     
-    axs[1].set_title('Corrente')
-    axs[1].set(ylabel='I')
-    axs[1].set(xlabel='t')
-    axs[1].grid()
-    axs[1].plot(t, J )
+    for i in neurons_that_fired[0]:
+        v[i] = c[i]
+        u[i] += d[i]
+    
+    I += np.expand_dims(np.sum(S[:, neurons_that_fired[0]], axis = 1), axis = 1)
+    
+    # Incrementing 0.5ms for numerical stability
+    v += 0.5*(0.04*v**2 + 5*v + 140 - u + I)
+    v += 0.5*(0.04*v**2 + 5*v + 140 - u + I)
+    u = u + a*(b*v - u)
+
+voltage_across_time = np.array(voltage_across_time)
+
+# Plots
+fig1, axs1 = plt.subplots(2, sharex=True)
+fig1.suptitle("Izhikevich model")
+axs1[0].plot(voltage_across_time, 'b', label='voltage (mV)')
+axs1[1].plot(I, 'k', label='input voltage (mV)')
+axs1[0].legend(shadow=True, fancybox=True)
+axs1[0].grid()
